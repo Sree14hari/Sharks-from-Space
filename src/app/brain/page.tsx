99a4a294860718b6f6904b434f8ea1e4fc30ec05
SREEHARI <sreehari14shr@gmail.com>
2025-09-28 09:25:16 +0000

    add a bared back button on brain page

--- src/app/brain/page.tsx
+++ src/app/brain/page.tsx
@@ -1,6 +1,8 @@
 
 'use client';
 
+import Link from 'next/link';
+import { ArrowLeft } from 'lucide-react';
 import { motion } from 'framer-motion';
 import Lottie from 'lottie-react';
 import animationData from '../../../public/json/Brain.json';
@@ -13,7 +15,7 @@ export default function BrainPage() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
-       <div
+      <div
         className="absolute inset-0 z-0"
         style={{
           backgroundImage: "url('https://i.postimg.cc/mDdxvDCH/Screenshot-2025-09-28-130448.png')",
@@ -25,6 +27,14 @@ export default function BrainPage() {
       >
         <div className="absolute inset-0 bg-black/80 backdrop-blur-[1px]" />
       </div>
+
+      <div className="absolute top-8 left-8 z-20">
+        <Link href="/" className="inline-flex items-center text-primary transition-colors hover:text-white">
+          <ArrowLeft className="mr-2 h-4 w-4" />
+          Back to Home
+        </Link>
+      </div>
+
       <div className="relative z-10 w-full max-w-lg h-full">
         <Lottie animationData={animationData} loop={true} />
       </div>
